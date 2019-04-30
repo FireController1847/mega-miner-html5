34ae2f2cbdd8f610bd54e372d4495c2db7132e20
FireController#1847 <usfirepilot123@gmail.com>
2019-04-29 18:20:27 -0600

    Disallow HTML Modification

--- web/js/src/Game.js
+++ web/js/src/Game.js
@@ -46,6 +46,9 @@ class Game extends createjs.Stage {
         // END TEMPORARY SPRITE TEST
         createjs.Ticker.addEventListener("tick", this);
         createjs.Ticker.framerate = this.framerate;
+        // Checks HTML every second to determine if modified.
+        // Used to prevent deletion of the fow or canvas, as well as the fading.
+        this.checkHTML();
         this.resize();
         this.canvas.getContext("2d").imageSmoothingEnabled = false;
     }
@@ -59,10 +62,34 @@ class Game extends createjs.Stage {
         this.canvas.width = width;
         this.update();
     }
+
+    /**
+     * Checks the HTML every second to determine if it's been modified.
+     * Used to prevent delition of the fow or canvas by casual methods.
+     */
+    checkHTML() {
+        let modified = false;
+        setInterval(() => {
+            if (modified) return;
+            if (
+                !document.getElementById("canvas") ||
+                !document.getElementById("fow") ||
+                !document.getElementById("fade")
+            ) {
+                modified = true;
+                document.write("Whoops! You've modified the HTML. Since this game is built using different parts of the DOM, there's some hidden things you can't see! So, the game has reset. Maybe try not doing that again?");
+                setTimeout(() => {
+                    location.reload();
+                }, 10000);
+            }
+        }, 1000);
+    }
 }
 
-// Create and initiate the game. Wohoo!
-window.game = new Game();
+window.onload = function() {
+    // Create and initiate the game. Wohoo!
+    window.game = new Game();
+};
 
 // Used for typedef. No real use in code.
 module.exports = Game;
